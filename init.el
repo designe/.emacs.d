102c8dda5d7f2dfbfe9143836781f49bcc493dad
Ji-woong Choi <hello.designe@facebook.com>
2016-09-18 14:54:08 +0900

    line highlight added

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 ;;
 ;; jbear's emacs configuration
 ;;
@@ -31,6 +32,38 @@
 (setq suggest-key-bindings t)
 (setq vc-follow-symlinks t)
 
+;; -- linum customizing --
+(defvar linum-current-line 1 "Current line number.")
+(defvar linum-border-width 1 "Border width for linum.")
+(defface linum-current-line
+  `((t :inherit linum
+       :forground "goldenrod"
+       :background "#555"
+       :weight bold
+       ))
+  "Face for displaying the current line number."
+  :group 'linum)
+
+(defadvice linum-update (before advice-linum-update activate)
+  "Set the current line."
+  (setq linum-current-line (line-number-at-pos)
+        linum-border-width (number-to-string
+                            (+ 1 (length
+                                  (number-to-string
+                                   (count-lines (point-min) (point-max))))))))
+
+(defun linum-highlight-current-line (line-number)
+  "Highlight the current line number using `linum-current-line` face."
+  (let ((face (if (= line-number linum-current-line)
+                  'linum-current-line
+                'linum)))
+    (propertize (format (concat "%" linum-border-width "d ") line-number)
+                'face face)))
+
+(setq linum-format 'linum-highlight-current-line)
+(global-linum-mode 1)
+
+
 ;; -- PACKAGE-ARCHIVES --
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
@@ -38,13 +71,14 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
-(setq jbear-packages '(better-defaults 
-                       smooth-scrolling 
+(setq jbear-packages '(better-defaults
+                       smooth-scrolling
                        python-mode
-                       jedi 
+                       jedi
                        js2-mode
                        js-comint
                        virtualenv
+                       magit
                        ))
 
 ;; -- JBEAR'S PACKAGE CHECK --
@@ -78,8 +112,21 @@
 
 ;;JS-MODE
 (load  "js-config.el")
-(add-to-list 'auto-mode-alist '("\\.js\\(on\\)?$" . js2-mode))
 
 ;; Zone Mode
 (require 'zone)
 (zone-when-idle 100)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yascroll magit zone-sl zone-rainbow zone-nyan virtualenv smooth-scrolling python-mode js2-mode js-comint jedi better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bold ((t (:weight extra-bold)))))
