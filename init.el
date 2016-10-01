0f30e6ee62b54f9c15d338cb0ac3cfcea0dec5b3
Ji-woong Choi <hello.designe@facebook.com>
2016-10-02 00:17:14 +0900

    split custom-line

--- init.el
+++ init.el
@@ -32,38 +32,6 @@
 (setq suggest-key-bindings t)
 (setq vc-follow-symlinks t)
 
-;; -- linum customizing --
-(defvar linum-current-line 1 "Current line number.")
-(defvar linum-border-width 1 "Border width for linum.")
-(defface linum-current-line
-  `((t :inherit linum
-       :forground "goldenrod"
-       :background "#555"
-       :weight bold
-       ))
-  "Face for displaying the current line number."
-  :group 'linum)
-
-(defadvice linum-update (before advice-linum-update activate)
-  "Set the current line."
-  (setq linum-current-line (line-number-at-pos)
-        linum-border-width (number-to-string
-                            (+ 1 (length
-                                  (number-to-string
-                                   (count-lines (point-min) (point-max))))))))
-
-(defun linum-highlight-current-line (line-number)
-  "Highlight the current line number using `linum-current-line` face."
-  (let ((face (if (= line-number linum-current-line)
-                  'linum-current-line
-                'linum)))
-    (propertize (format (concat "%" linum-border-width "d ") line-number)
-                'face face)))
-
-(setq linum-format 'linum-highlight-current-line)
-(global-linum-mode 1)
-
-
 ;; -- PACKAGE-ARCHIVES --
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
@@ -93,6 +61,10 @@
       package)))
 
 (package-initialize)
+
+;;CUSTOM-LINUM
+(load "custom-linum.el")
+(global-linum-mode 1)
 
 ;;BETTER-DEFAULTS
 (require 'better-defaults)
